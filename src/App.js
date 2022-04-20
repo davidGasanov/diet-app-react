932765f1ed572c49d471ae6f82edb97b1df0798a
LordOfTheGame <45798137+LordOfTheGame@users.noreply.github.com>
2022-04-20 08:13:04 +0400

    feat: create registration folder

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 import React from "react";
 import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
-import { Stack, Container, Typography, Box } from "@mui/material";
-import RegistrationTextinput from "./components/RegistrationTextinput";
+import Registration from "./components/pages/Registration"
 
 function App() {
   return (
     <div className="App">
-      <Container sx={{ display: "flex", justifyContent: "center" }}>
-        <Box
-          component="form"
-          maxWidth="sm"
-          sx={{
-            boxShadow: 1,
-            width: 300,
-            padding: 2,
-          }}
-        >
-          <Typography gutterBottom align="center" variant="h3" component="h1">
-            Welcome
-          </Typography>
-          <Stack spacing={2}>
-            <RegistrationTextinput
-              label="Username"
-              helperText="Must begin with a letter and be at least 5-characters long (excluding special characters such as #, $, % etc.)."
-              validation={/^\w[a-zA-Z0-9]{3,}[a-zA-Z0-9]$/}
-            />
-            <RegistrationTextinput
-              label="Email"
-              helperText="Must be a valid email"
-              validation={/^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+[.][a-z]+$/}
-            />
-            <RegistrationTextinput
-              label="Password"
-              helperText="Must be at least 8 characters long, including at least 1 uppercase character and at least 1 special character."
-              validation={/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\.@$!%*?&])[A-Za-z\d@.$!%*?&]{8,}$/}
-       />
-
-            
-          </Stack>
-        </Box>
-      </Container>
+     <Registration/>
     </div>
   );
 }
