b4f9cfd2182e2cf50dad5a65589388a9d077d48c
LordOfTheGame <45798137+davidGasanov@users.noreply.github.com>
2022-04-27 08:41:55 +0400

    feat: add posts page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 // import CssBaseline from "@mui/material/CssBaseline";
 import Registration from "./Registration";
+import Posts from "./Posts";
 import Login from "./Login";
 
 import { Route, Routes } from "react-router-dom";
@@ -10,8 +11,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/userposts" element={<Posts />} />
         <Route path="/Register" element={<Registration />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </div>
   );
